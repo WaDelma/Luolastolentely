9b3ad043dba55b3dab764f2c263e7513eab5cc82
WaDelma <aopkarja@dhp-20.cs.helsinki.fi>
2013-01-15 17:27:44 +0200

    Perus LWJGL:ää... Sain jo piirrettyä keltaisen neliön! :D En ole varma rakenteesta... Renderöinti pitää tehdä kai viimeisenä...

--- Luolastolentely/src/aopkarja/UI/UIKasittelija.java
+++ Luolastolentely/src/aopkarja/UI/UIKasittelija.java
@@ -2,6 +2,13 @@ package aopkarja.UI;
 
 import aopkarja.Kasittelija;
 import aopkarja.KasittelyTyyppi;
+import java.awt.Canvas;
+import javax.swing.JFrame;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.GLU;
 
 /**
  *
@@ -10,17 +17,34 @@ import aopkarja.KasittelyTyyppi;
 public class UIKasittelija {
 
     @Kasittelija(KasittelyTyyppi.KAYNNISTA)
-    public void initialisoi() {
-        
+    public void initialisoi() throws LWJGLException {
+        Display.setDisplayMode(new DisplayMode(800, 600));
+        Display.create();
+
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
     }
-    
+
     @Kasittelija(KasittelyTyyppi.AJA)
     public void aja() {
-        
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        GL11.glColor3f(1.0f, 1.0f, 0.0f);
+        GL11.glBegin(GL11.GL_QUADS);
+        {
+            GL11.glVertex2f(100, 100);
+            GL11.glVertex2f(100 + 200, 100);
+            GL11.glVertex2f(100 + 200, 100 + 200);
+            GL11.glVertex2f(100, 100 + 200);
+        }
+        GL11.glEnd();
+        Display.update();
+        Display.sync(60);
     }
 
     @Kasittelija(KasittelyTyyppi.LOPETA)
     public void sulje() {
-        
+        Display.destroy();
     }
 }
